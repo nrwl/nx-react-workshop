556ad83c1c1c69e3bf009c5ca358d574ecad6661
Isaac Mann <isaacplmann+git@gmail.com>
2021-04-15 15:32:28 -0400

    lab8

--- libs/store/feature-game-detail/src/lib/store-feature-game-detail.tsx
+++ libs/store/feature-game-detail/src/lib/store-feature-game-detail.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import styles from './store-feature-game-detail.module.scss';
 
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import CardMedia from '@material-ui/core/CardMedia';
+import { formatRating } from '@bg-hoard/store/util-formatters';
 
 type TParams = { id: string };
 
@@ -14,17 +19,76 @@ export interface StoreFeatureGameDetailProps
   extends RouteComponentProps<TParams> {}
 
 export const StoreFeatureGameDetail = (props: StoreFeatureGameDetailProps) => {
+  const [state, setState] = useState<{
+    data: any;
+    loadingState: 'success' | 'error' | 'loading';
+  }>({
+    data: {},
+    loadingState: 'success',
+  });
+
+  useEffect(() => {
+    setState({
+      ...state,
+      loadingState: 'loading',
+    });
+    const gameId = props.match.params.id;
+    fetch(`/api/games/${gameId}`)
+      .then((x) => x.json())
+      .then((res) => {
+        setState({
+          ...state,
+          data: res,
+          loadingState: 'success',
+        });
+      })
+      .catch((err) => {
+        setState({
+          ...state,
+          loadingState: 'error',
+        });
+      });
+  }, [props.match.params.id]);
+
   return (
-    <div className={styles.container}>
-      <Card>
-        <CardActionArea>
+    <div className={styles['container']}>
+      {state.loadingState === 'loading' ? (
+        'Loading...'
+      ) : state.loadingState === 'error' ? (
+        <div>Error fetching data</div>
+      ) : state.data == null ? (
+        ''
+      ) : (
+        <Card>
+          <CardHeader title={state.data.name}></CardHeader>
+          {state.data.image ? (
+            <CardMedia
+              className={styles['game-card-media']}
+              image={state.data.image}
+              title={state.data.name}
+            />
+          ) : null}
           <CardContent>
             <Typography variant="body2" color="textSecondary" component="p">
-              {props.match.params.id}
+              {state.data.description}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+              className={styles['game-rating']}
+            >
+              <strong>Rating:</strong> {formatRating(state.data.rating)}
             </Typography>
           </CardContent>
-        </CardActionArea>
-      </Card>
+          <CardActions>
+            <Button variant="contained" color="primary">
+              Buy
+            </Button>
+            <Button variant="contained">Share</Button>
+          </CardActions>
+        </Card>
+      )}
     </div>
   );
 };
