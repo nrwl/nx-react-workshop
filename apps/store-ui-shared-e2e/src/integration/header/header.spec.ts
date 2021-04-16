1f7929ac027ce7eadec35cbb707fe5c78c9bf778
Isaac Mann <isaacplmann+git@gmail.com>
2021-04-16 12:06:07 -0400

    lab11

--- apps/store-ui-shared-e2e/src/integration/header/header.spec.ts
+++ apps/store-ui-shared-e2e/src/integration/header/header.spec.ts
@@ -1,7 +1,7 @@
 describe('store-ui-shared: Header component', () => {
   beforeEach(() => cy.visit('/iframe.html?id=header--primary'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to store-ui-shared!');
-    });
+
+  it('should show the title', () => {
+    cy.get('h6').contains('Board Game Hoard');
+  });
 });
