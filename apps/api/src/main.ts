a6e91de54b4771bec8d8051a91a69212bfa444f7
Rares Matei <matei.rar@gmail.com>
2021-08-10 19:44:51 +0100

    finish lab 19 - path 2

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,10 +4,12 @@
  */
 
 import * as express from 'express';
+import * as cors from 'cors';
 import { getAllGames, getGame } from './app/games.repository';
 
 const app = express();
 
+app.use(cors());
 app.get('/api/games', (req, res) => {
   res.send(getAllGames());
 });
@@ -16,7 +18,7 @@ app.get('/api/games/:id', (req, res) => {
   return res.send(getGame(req.params.id));
 });
 
-const port = process.env.port || 3333;
+const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
