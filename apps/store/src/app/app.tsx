a4ed7ad71ee528a8c15662ef9d3d6e5282987fd1
Rares Matei <matei.rar@gmail.com>
2021-08-10 18:38:16 +0100

    finish lab 21 - path 1

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -16,83 +16,51 @@ import { formatRating } from '@bg-hoard/store/util-formatters';
 import { Route, useHistory } from 'react-router-dom';
 
 import { StoreFeatureGameDetail } from '@bg-hoard/store/feature-game-detail';
+import { getAllGames } from '../fake-api/index';
 
 export const App = () => {
   const history = useHistory();
-  const [state, setState] = useState<{
-    data: Game[];
-    loadingState: 'success' | 'error' | 'loading';
-  }>({
-    data: [],
-    loadingState: 'success',
-  });
-
-  useEffect(() => {
-    setState((state) => ({
-      ...state,
-      loadingState: 'loading',
-    }));
-    fetch('/api/games')
-      .then((x) => x.json())
-      .then((res) => {
-        setState((state) => ({
-          ...state,
-          data: res,
-          loadingState: 'success',
-        }));
-      })
-      .catch((err) => {
-        setState((state) => ({
-          ...state,
-          loadingState: 'error',
-        }));
-      });
-  }, []);
 
   return (
     <>
       <Header title="Board Game Hoard" />
       <div className={styles.container}>
         <div className={styles['games-layout']}>
-          {state.loadingState === 'loading'
-            ? 'Loading...'
-            : state.loadingState === 'error'
-            ? '<div>Error retrieving data</div>'
-            : state.data.map((x) => (
-                <Card
-                  key={x.id}
-                  className={styles['game-card']}
-                  onClick={() => history.push(`/game/${x.id}`)}
-                >
-                  <CardActionArea>
-                    <CardMedia
-                      className={styles['game-card-media']}
-                      image={x.image}
-                      title={x.name}
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {x.name}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        {x.description}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        component="p"
-                        className={styles['game-rating']}
-                      >
-                        <strong>Rating:</strong> {formatRating(x.rating)}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-              ))}
+          {getAllGames().map((x) => (
+            <Card
+              key={x.id}
+              className={styles['game-card']}
+              onClick={() => history.push(`/game/${x.id}`)}
+            >
+              <CardActionArea>
+                <CardMedia
+                  className={styles['game-card-media']}
+                  image={x.image}
+                  title={x.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {x.name}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {x.description}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                    className={styles['game-rating']}
+                  >
+                    <strong>Rating:</strong> {formatRating(x.rating)}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
         </div>
         <Route path="/game/:id" component={StoreFeatureGameDetail} />
       </div>
