580ffed2b9adedceaef47e9e0e71b1e13e97f7f1
Miroslav Jonas <missing.manual@gmail.com>
2021-08-10 14:07:20 +0200

    chore(deps): update nx to 12.6.5

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/store',
-    '<rootDir>/libs/store/ui-shared',
-    '<rootDir>/libs/store/util-formatters',
-  ],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
