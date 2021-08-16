77d98df23495c836f8711414f3d20f1744b5a6d2
Miroslav Jonas <missing.manual@gmail.com>
2021-08-16 13:59:45 +0200

    fix examples

--- examples/lab6/libs/store/feature-game-detail/src/lib/store-feature-game-detail/store-feature-game-detail.tsx
+++ examples/lab6/libs/store/feature-game-detail/src/lib/store-feature-game-detail/store-feature-game-detail.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-import styles from './game-detail.module.scss';
+import styles from './store-feature-game-detail.module.scss';
 
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -11,7 +10,7 @@ type TParams = { id: string };
 
 /* eslint-disable-next-line */
 export interface StoreFeatureGameDetailProps
-  extends RouteComponentProps<TParams> { }
+  extends RouteComponentProps<TParams> {}
 
 export const StoreFeatureGameDetail = (props: StoreFeatureGameDetailProps) => {
   return (
